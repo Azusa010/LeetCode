e4219b272b2f7caf87905bb0dbda42be5fd51e60
Azusa <m3171470760@gmail.com>
2026-07-22 17:49:36 +0800

    206

---
+++ 206_反转链表.py
@@ -0,0 +1,31 @@
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pre = None
+        cur = head
+        while cur is not None:
+            temp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = temp
+        return pre
+        
+l1 = ListNode(1)
+l2 = ListNode(2)
+l3 = ListNode(3)
+l4 = ListNode(4)
+l5 = ListNode(5)
+l1.next=l2
+l2.next=l3
+l3.next=l4
+l4.next=l5
+reversed_head = Solution().reverseList(l1)
+cur = reversed_head
+while cur:
+    print(cur.val, end=" ")
+    cur = cur.next
